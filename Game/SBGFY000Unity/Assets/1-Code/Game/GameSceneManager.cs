b1e7f9a61c7b250d023daf8f5dac635e5d03f80f
Fellow Incognito <fellowincognito@gmail.com>
2015-05-07 00:55:53 -0500

    Added initial GameSceneManager which will handle much of the local client game state for things like input and pending actions. Perhaps not the best, but will most likely work.

---
+++ Game/SBGFY000Unity/Assets/1-Code/Game/GameSceneManager.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * StarBase GFY000
+ * GameSceneManager
+ * Handles the local game scene for the player, including mouse stuff, and whatever game state things
+ * Created 7 May 2015
+ * Author: Fellow Incognito (fellowincognito@gmail.com)
+ * SEE LICENSE FOR TERMS AND RIGHTS
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class GameSceneManager : MonoBehaviour
+{
+    #region Enums
+    public enum LocalPlayerState
+    {
+        None,
+        Administrator,
+        Player
+    }
+
+    public enum AdministratorMode
+    {
+        None,
+        GroundSelection,
+        ObjectSelection,
+        GroundPlacement,
+        ObjectPlacement
+    }
+
+    public enum PlayerMode
+    {
+        None,
+        Moving
+    }
+    #endregion
+
+    #region Vars
+    protected LocalPlayerState m_localPlayerState;
+    protected AdministratorMode m_adminModeState;
+    protected PlayerMode m_playerModeState;
+    #endregion
+}
